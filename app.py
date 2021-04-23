d983d7f9596a444ff922ce0ff209a366a29b0776
jaiminpanchal19 <69243648+jaiminpanchal19@users.noreply.github.com>
2021-04-23 12:32:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 """
 Created on Thu Apr 15 01:03:50 2021
 
+author: jaimin panchal
 """
 
 
@@ -23,18 +24,19 @@ def predict_price(Year, Present_Price, Kms_Driven, Owner, Fuel_Type_Diesel, Fuel
 
 def main():
     html_temp = """
-    <div style="background-color:#025246 ;padding:10px">
-    <h2 style="color:white;text-align:center;">Car Price Prediction ML App </h2>
-    </div>
-    """
+        <div style="background-color: #3cba54 ; padding:  10px; border-radius: 10px">
+          <h1 style="color:white; text-align: center">Car Price Predictor</h1>
+        </div>
+     """
     st.markdown(html_temp, unsafe_allow_html=True)
 
     Year = st.number_input("Year Of Purchase")
-    Present_Price = st.number_input("Present Price")
+    Present_Price = st.number_input("Present Price (in lacs)")
     Kms_Driven = st.number_input("Kms Driven")
     Owner = st.number_input("Number of owners")
     
     fueltype = st.selectbox("Select the fuel type", ['Petrol', 'CNG', 'Diesel'])
+    
     Fuel_Type_Diesel = 0
     Fuel_Type_Petrol = 0
     if(fueltype == 'CNG'):
@@ -49,7 +51,7 @@ def main():
     
     
     Seller_Type_Individual = 0
-    sellertype = st.selectbox("Select the seller type", ['Dealer', 'Individual'])
+    sellertype = st.radio("Select the seller type", ('Dealer', 'Individual'))
     if(sellertype == 'Dealer'):
         Seller_Type_Individual = 0
         
@@ -58,7 +60,7 @@ def main():
     
     
     Transmission_Manual = 0
-    trans = st.selectbox("Select the transmission type", ['Manual', 'Automatic'])
+    trans = st.radio("Select the transmission type", ('Manual', 'Automatic'))
     if(trans == 'Manual'):
         Transmission_Manual = 1
         
@@ -72,7 +74,7 @@ def main():
         output=predict_price(Year, Present_Price, Kms_Driven, Owner, Fuel_Type_Diesel, Fuel_Type_Petrol, Seller_Type_Individual, Transmission_Manual)
         
         output = round(output, 2)
-        st.success('The selling price is {} lacs'.format(str(output)))
+        st.success('The selling price is {} Lacs'.format(str(output)))
 
 
 if __name__=='__main__':
